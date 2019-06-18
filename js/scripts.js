e827b88634c2f37bf586b10508136d35caea1d4e
melisajuma <Melisaakinyi95@gmail.com>
2019-06-18 09:38:11 +0300

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,11 @@ var topping = ["Chicken", "Vegetables", "Beef"];
 //calculating the price of an order depending on customer specification
 order.prototype.pizzaCost = function () {
     if (this.size == "Small") {
-        this.price = 0 + 400;
+        this.price += 400;
     } else if (this.size == "Medium") {
-        this.price = 0 + 600;
+        this.price += 600;
     } else if (this.size == "Large") {
-        this.price = 0 + 800;
+        this.price +=  800;
     }
     if (this.crust == "Glutten-free") {
         this.price += 50;
@@ -33,26 +33,34 @@ order.prototype.pizzaCost = function () {
     }
     return this.price;
 }
+order.prototype.totalCostOfOrders= function () {
+    var shoppingCartTotal = 0;
+    for (var order = 0; order < totalOrderPrice.length; order++) {
+        shoppingCartTotal += totalOrderPrice[order];
+    }
+    return shoppingCartTotal;
+}
 
 var totalCostOfOrders = [];
 
 $(document).ready(function () {
-    $("#submission").submit(function (event) {
+    $("form#submission").submit(function (event) {
         event.preventDefault();
         var size = $("select#Size").val()
         var crust = $("input#crust").val();
-        var topping = $("input.topping").val();
+        var topping = $("input#topping").val();
 
     var newPizzaOrder = new order(size, crust, topping);
+    newPizzaOrder.pizzaCost();
     totalCostOfOrders.push(newPizzaOrder.price);
 
     $("#order-cost").show();
-    $("#order-cost").append("\t" + "\t" + size);
-    $("#order-cost").append("\t" + "\t" + crust);
-    $("#order-cost").append("\t" + "\t" + topping);
-    $("#final-cost").text("\t" + "\t" + newPizzaOrder.totalCostOfOrders());
+    $("#pizzaSize").append("\t" + "\t" + size);
+    $("#pizzaCrust").append("\t" + "\t" + crust);
+    $("#pizzaTopping").append("\t" + "\t" + topping);
+    $("#order-cost").append("\t" + "\t" + newPizzaOrder.totalCostOfOrders());
      });     
-     $("#submitbutton").click(function(){
+     $("").click(function(){
            location.reload();
         });
 
